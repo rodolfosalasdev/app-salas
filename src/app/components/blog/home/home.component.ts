ccac3dafb7d94d91840b78cd0a12b12e957fc23c
Rodolfo Salas de Lima <rodolfosalas@Rodolfos-MacBook-Pro.local>
2024-06-07 11:54:10 -0300

    feat: creating blog home-page

--- src/app/components/blog/home/home.component.ts
+++ src/app/components/blog/home/home.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class HomeComponent {
+
+  cards = [
+    { title: 'title 1',
+      contentTitle: 'Special title treatment',
+      content: '', 
+      action: ''
+    },
+    { title: 'title 2', 
+      contentTitle: 'Special title treatment',
+      content: 'With supporting text below as a natural lead-in to additional content.', 
+      action: 'Go somewhere'
+    },
+    { title: 'title 3',
+      contentTitle: 'Special title treatment',
+      content: '', 
+      action: ''
+    },
+  ]
 
 }
