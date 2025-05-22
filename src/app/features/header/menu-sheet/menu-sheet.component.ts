a395f1f540e0d71d0ce2cd6ce40307d22ed20495
Rodolfo Salas de Lima <rodolfosalas@Rodolfos-MacBook-Pro.local>
2025-05-22 10:33:36 -0300

    feat: add socal media and close sheet

--- src/app/features/header/menu-sheet/menu-sheet.component.ts
+++ src/app/features/header/menu-sheet/menu-sheet.component.ts
@@ -1,44 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
 
 import { provideIcons, NgIcon } from '@ng-icons/core';
-import { lucideCross, lucideMail } from '@ng-icons/lucide';
+import { lucideChartBar, lucideMail } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
-import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
-import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
-import { BrnSheetContentDirective, BrnSheetTriggerDirective } from '@spartan-ng/brain/sheet';
-import {
-  HlmSheetComponent,
-  HlmSheetContentComponent,
-  HlmSheetDescriptionDirective,
-  HlmSheetFooterComponent,
-  HlmSheetHeaderComponent,
-  HlmSheetTitleDirective
-} from '@spartan-ng/ui-sheet-helm';
+import { BrnSheetComponent, BrnSheetContentDirective, BrnSheetTriggerDirective } from '@spartan-ng/brain/sheet';
+import { HlmSheetComponent, HlmSheetContentComponent, HlmSheetFooterComponent } from '@spartan-ng/ui-sheet-helm';
 import { TranslateModule } from '@ngx-translate/core';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 import { lucideMenu } from '@ng-icons/lucide';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-sheet',
   standalone: true,
   imports: [
+    CommonModule,
     TranslateModule,
     BrnSheetTriggerDirective,
     BrnSheetContentDirective,
     HlmSheetComponent,
     HlmSheetContentComponent,
-    HlmSheetHeaderComponent,
     HlmSheetFooterComponent,
-    HlmSheetTitleDirective,
-    HlmSheetDescriptionDirective,
     HlmButtonDirective,
-    HlmInputDirective,
-    HlmLabelDirective,
     HlmIconDirective,
     NgIcon
   ],
-  providers: [provideIcons({ lucideMenu, lucideMail })],
+  providers: [provideIcons({ lucideMenu, lucideMail, lucideChartBar })],
   templateUrl: './menu-sheet.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MenuSheetComponent {}
+export class MenuSheetComponent {
+  public viewchildSheetRef = viewChild(BrnSheetComponent);
+
+  closeSheet() {
+    this.viewchildSheetRef()?.close({});
+  }
+}
--- src/app/features/header/profile-sheet/profile-sheet.component.ts
+++ src/app/features/header/profile-sheet/profile-sheet.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
 
-import { provideIcons } from '@ng-icons/core';
-import { lucideCross } from '@ng-icons/lucide';
+import { provideIcons, NgIcon } from '@ng-icons/core';
+import { lucideCross, lucideInstagram, lucideLinkedin } from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
@@ -16,6 +16,19 @@ import {
 } from '@spartan-ng/ui-sheet-helm';
 import { HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective } from '@spartan-ng/ui-avatar-helm';
 import { TranslateModule } from '@ngx-translate/core';
+
+import {
+  BrnPopoverCloseDirective,
+  BrnPopoverComponent,
+  BrnPopoverContentDirective,
+  BrnPopoverTriggerDirective
+} from '@spartan-ng/brain/popover';
+import { HlmPopoverCloseDirective, HlmPopoverContentDirective } from '@spartan-ng/ui-popover-helm';
+
+import { HlmSeparatorDirective } from '@spartan-ng/ui-separator-helm';
+import { BrnSeparatorComponent } from '@spartan-ng/brain/separator';
+
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-profile-sheet',
@@ -35,10 +48,29 @@ import { TranslateModule } from '@ngx-translate/core';
     HlmLabelDirective,
     HlmAvatarImageDirective,
     HlmAvatarComponent,
-    HlmAvatarFallbackDirective
+    HlmAvatarFallbackDirective,
+    BrnPopoverCloseDirective,
+    BrnPopoverComponent,
+    BrnPopoverContentDirective,
+    BrnPopoverTriggerDirective,
+    HlmPopoverCloseDirective,
+    HlmPopoverContentDirective,
+    HlmSeparatorDirective,
+    BrnSeparatorComponent,
+    HlmIconDirective,
+    NgIcon
   ],
-  providers: [provideIcons({ lucideCross })],
+  providers: [provideIcons({ lucideCross, lucideInstagram, lucideLinkedin })],
   templateUrl: './profile-sheet.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileSheetComponent {}
+export class ProfileSheetComponent {
+  public socialMedia = {
+    instagram: 'https://www.instagram.com/rodolfosalas.dev/',
+    linkedIn: 'https://www.linkedin.com/in/rodolfo-salas-21858911b/'
+  };
+
+  public redirectTo(media: string): void {
+    window.open(media, '_blank');
+  }
+}
