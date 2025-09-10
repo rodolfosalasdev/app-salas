078535cc74ec5bc4f1f042cd57d83978580d8273
Rodolfo Salas <rodolfosalas@MacBook-Pro-de-Rodolfo.local>
2025-09-10 18:29:27 -0300

    chore: eslint and prettier config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules'],
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+    },
+    settings: eslintConfigPrettier, // Desativa regras de formatação do ESLint
+  },
+];
